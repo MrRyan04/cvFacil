1b133b60d7a6caa8a512053c5c0576da8fbb75ec
Mr Ryan <ranieryryan01@gmail.com>
2026-05-16 20:16:03 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const pageRoutes = require("./routes/pageRoutes");
+
+app.use("/", pageRoutes);
+
+module.exports = app;
