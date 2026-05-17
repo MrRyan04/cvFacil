35a18363ffe5e9f311e668bc0e59430eccf5fbe0
Mr Ryan <ranieryryan01@gmail.com>
2026-05-16 21:46:41 -0300

    loginPage prot

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,3 +1,7 @@
 exports.home = (req, res) => {
   res.render("home");
 };
+
+exports.login = (req, res) => {
+  res.render("login");
+};
--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const pageController = require("../controllers/pageController");
 
 router.get("/", pageController.home);
+router.get("/login", pageController.login);
 
 module.exports = router;
