c6764695b354a03e6675d9555ac7edbed297d5e1
Mr Ryan <ranieryryan01@gmail.com>
2026-05-21 21:07:03 -0300

    data 1

--- server.js
+++ server.js
@@ -2,4 +2,19 @@ const app = require("./src/app");
 
 app.listen(3000, () => {
   console.log("Servidor rodando");
-});
+});const app = require("./src/app");
+const sequelize = require("./src/config/database");
+
+require("./src/models/User");
+
+const PORT = 3000;
+
+sequelize.sync()
+    .then(() => {
+        console.log("Banco conectado");
+
+        app.listen(PORT, () => {
+            console.log("Servidor rodando");
+        });
+    })
+    .catch(err => console.log(err));
