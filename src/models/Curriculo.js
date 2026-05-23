d467e517d77cc7295289fda87b33b0eea9778405
Mr Ryan <ranieryryan01@gmail.com>
2026-05-23 02:45:29 -0300

    model cv

---
+++ src/models/Curriculo.js
@@ -0,0 +1,59 @@
+const {
+    DataTypes
+} = require("sequelize");
+
+const sequelize =
+    require("../config/database");
+
+const Curriculo =
+    sequelize.define(
+        "Curriculo",
+        {
+
+            nome: {
+                type:
+                    DataTypes.STRING,
+                allowNull: false
+            },
+
+            email: {
+                type:
+                    DataTypes.STRING,
+                allowNull: false
+            },
+
+            telefone: {
+                type:
+                    DataTypes.STRING
+            },
+
+            cidade: {
+                type:
+                    DataTypes.STRING
+            },
+
+            objetivo: {
+                type:
+                    DataTypes.TEXT
+            },
+
+            sobre: {
+                type:
+                    DataTypes.TEXT
+            },
+
+            formacao: {
+                type:
+                    DataTypes.TEXT
+            },
+
+            experiencia: {
+                type:
+                    DataTypes.TEXT
+            }
+
+        }
+    );
+
+module.exports =
+    Curriculo;
